dba9276f273328bf069777990526be43b0cf8f3d
KJH <khjjh1445@gmail.com>
2020-11-17 17:56:33 +0900

    React로 만든 가위바위보

---
+++ 5.가위바위보/webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const RefreshWebpackplugin = require('@pmmmwh/react-refresh-webpack-plugin');
+
+module.exports = {
+    name: 'word-relay-dev',
+    mode: 'development',
+    devtool: 'inline-source-map',
+    resolve: {
+      extensions: ['.js', '.jsx'],
+    },
+
+    entry: {
+        app: ['./client'],
+    }, // 입력
+
+    module: {
+        rules: [{
+            test: /\.jsx$/, //js, jsx 파일 룰 적용 (정규표현식)
+            loader: 'babel-loader',
+            options: {
+                presets: [
+                    ['@babel/preset-env', {
+                        targets: {
+                            browsers: ['> 1% in KR'], // browserslist
+                        },  
+                        debug: true,
+                    }],
+                    '@babel/preset-react'
+                ],
+                plugins: [
+                    '@babel/plugin-proposal-class-properties',
+                    'react-hot-loader/babel',
+                ],
+            },
+        }],
+    },
+    plugins: [
+        new RefreshWebpackplugin()
+    ],
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'app.js',
+        publicPath: '/dist/',
+    }, // 출력
+    devServer: {
+        publicPath: '/dist/',
+        hot: true,
+    },
+};
