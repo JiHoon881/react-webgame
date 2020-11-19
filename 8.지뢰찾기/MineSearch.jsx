1ae84b35a99f6e1ead2d33229f27e118d1d4e982
KJH <khjjh1445@gmail.com>
2020-11-19 17:36:05 +0900

    React로 만든 지뢰찾기

--- 8.지뢰찾기/MineSearch.jsx
+++ 8.지뢰찾기/MineSearch.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useMemo, useReducer } from 'react';
+import React, { createContext, memo, useEffect, useMemo, useReducer } from 'react';
 import Form from './Form';
 import Table from './Table';
 
@@ -15,13 +15,21 @@ export const CODE = {
 
 export const TableContext = createContext({
     tableData: [],
+    halted: true,
     dispatch: () => {},
 });
 
 const initialState = {
     tableData: [],
+    data: {
+        row: 0,
+        cell: 0,
+        mine: 0,
+    },
     timer: 0,
     result: '',
+    halted: true,
+    openedCount: 0,
 };
 
 const plantMine = (row, cell, mine) => {
@@ -29,11 +37,13 @@ const plantMine = (row, cell, mine) => {
     const candidate = Array(row * cell).fill().map((arr, i) => {
         return i;
     });
+
     const shuffle = [];
     while (candidate.length > row * cell - mine) {
         const chosen = candidate.splice(Math.floor(Math.random() * candidate.length), 1)[0];
         shuffle.push(chosen);
     }
+
     const data = [];
     for (let i = 0; i < row; i++) {
         const rowData = [];
@@ -42,43 +52,222 @@ const plantMine = (row, cell, mine) => {
             rowData.push(CODE.NORMAL);
         }
     }
-    for (let k = 0; k < shuffle.length; k++) {
+
+    for (let k = 0; k < shuffle.length; k++) {  
         const ver = Math.floor(shuffle[k] / cell);
         const hor = shuffle[k] % cell ;
         data[ver][hor] = CODE.MINE;
     }
+
     console.log(data);
     return data;
 };
 
 export const START_GAME = 'START_GAME';
+export const OPEN_CELL = 'OPEN_CELL';
+export const CLICK_MINE = 'CLICK_MINE';
+export const FLAG_CELL = 'FLAG_CELL'; // 깃발
+export const QUESTION_CELL = 'QUESTION_CELL'; // 물음표
+export const NORMALIZE_CELL = 'NORMALIZE_CELL'; // 보통
+export const INCREMENT_TIMER = 'INCREMENT_TIMER';
 
 const reducer = (state, action) => {
     switch (action.type) {
         case START_GAME:
             return {
                 ...state,
-                tableData: plantMine(action.row, action.cell, action.mine)
+                data: {
+                    row: action.row,
+                    cell: action.cell,
+                    mine: action.mine,
+                },
+                openedCount: 0,
+                tableData: plantMine(action.row, action.cell, action.mine),
+                halted: false,
+                timer: 0,
             };
+
+        case OPEN_CELL: {
+            const tableData = [...state.tableData];
+            tableData.forEach((row, i) => {
+                tableData[i] = [...state.tableData[i]];
+            });
+            
+            const checked = [];
+            let openedCount = 0;
+            console.log(tableData.length, tableData[0].length);
+            const checkArround = (row, cell) => {
+                console.log(row, cell);
+                if (row < 0 || row >= tableData.length || cell < 0 || cell >= tableData[0].length) { // 상하좌우 칸이 아닌 경우 필터링
+                    return;
+                } // 상하좌우 없는칸은 안 열기
+                
+                if ([CODE.OPENED, CODE.FLAG_MINE, CODE.FLAG, CODE.QUESTION_MINE, CODE.QUESTION].includes(tableData[row][cell])){
+                    return;
+                } // 단힌 칸만 열기
+
+                if (checked.includes(row + '/' + cell)) {
+                    return;
+                } else {
+                    checked.push(row + '/' + cell);
+                } // 한 번 연칸은 무시하기
+
+                let around = [
+                    tableData[row][cell - 1], tableData[row][cell + 1],
+                ];
+
+                if (tableData[row - 1]) { // 주변 지뢰 갯수 검사
+                    around = around.concat([tableData[row - 1][cell - 1], tableData[row - 1][cell], tableData[row - 1][cell + 1]]);
+                }
+
+                if (tableData[row + 1]) {
+                    around = around.concat([tableData[row + 1][cell - 1], tableData[row + 1][cell], tableData[row + 1][cell + 1]]);
+                }
+                
+                const count = around.filter(function (v) {
+                    return [CODE.MINE, CODE.FLAG_MINE, CODE.QUESTION_MINE].includes(v);
+                }).length;
+
+                if (count === 0) { // 주변칸 오픈
+                    if (row > -1) {
+                        const near = [];
+                        if (row - 1 > -1) {
+                            near.push([row - 1, cell - 1]);
+                            near.push([row - 1, cell]);
+                            near.push([row - 1, cell + 1]);
+                        }
+
+                        near.push([row, cell - 1]);
+                        near.push([row, cell + 1]);
+
+                        if (row + 1 < tableData.length) {
+                            near.push([row + 1, cell - 1]);
+                            near.push([row + 1, cell]);
+                            near.push([row + 1, cell + 1]);
+                        }
+
+                        near.filter(v => !!v).forEach((n) => {
+                            if (tableData[n[0]][n[1]] !== CODE.OPENED){
+                                checkArround(n[0], n[1]);
+                            }
+                        })
+                    }
+                }
+                if (tableData[row][cell] === CODE.NORMAL) { // 내 칸이 닫힌 칸이면 카운트 증가
+                    openedCount += 1;
+                }
+                tableData[row][cell] = count;
+            };
+
+            checkArround(action.row, action.cell);
+            let halted = false;
+            let result = '';
+            console.log(state.data.row * state.data.cell - state.data.mine, state.openedCount, openedCount);
+            if (state.data.row * state.data.cell - state.data.mine === state.openedCount + openedCount) { // 승리
+                halted = true;
+                result = `${state.timer}초만에 승리하셨습니다.`;
+            }
+            return {
+                ...state,
+                tableData,
+                openedCount: state.openedCount + openedCount,
+                halted,
+                result,
+            };
+        }
+
+        case CLICK_MINE: {
+            const tableData = [...state.tableData];
+            tableData[action.row] = [...state.tableData[action.row]];
+            tableData[action.row][action.cell] = CODE.CLICKED_MINE;
+            return {
+                ...state,
+                tableData,
+                halted: true,
+            };
+        }
+
+        case FLAG_CELL: {
+            const tableData = [...state.tableData];
+            tableData[action.row] = [...state.tableData[action.row]];
+            if (tableData[action.row][action.cell] === CODE.MINE) {
+                tableData[action.row][action.cell] = CODE.FLAG_MINE;
+            } else {
+                tableData[action.row][action.cell] = CODE.FLAG;
+            }
+            return {
+                ...state,
+                tableData,
+            };
+        }
+
+        case QUESTION_CELL: {
+            const tableData = [...state.tableData];
+            tableData[action.row] = [...state.tableData[action.row]];
+            if (tableData[action.row][action.cell] === CODE.FLAG_MINE) {
+                tableData[action.row][action.cell] = CODE.QUESTION_MINE;
+            } else {
+                tableData[action.row][action.cell] = CODE.QUESTION;
+            }
+            return {
+                ...state,
+                tableData,
+            };
+        }
+
+        case NORMALIZE_CELL: {
+            const tableData = [...state.tableData];
+            tableData[action.row] = [...state.tableData[action.row]];
+            if (tableData[action.row][action.cell] === CODE.QUESTION_MINE) {
+                tableData[action.row][action.cell] = CODE.MINE;
+            } else {
+                tableData[action.row][action.cell] = CODE.NORMAL;
+            }
+            return {
+                ...state,
+                tableData,
+            };
+        }
+
+        case INCREMENT_TIMER: {
+            return {
+                ...state,
+                timer: state.timer + 1,
+            }
+        }
+
         default:
             return state;
     }
 };
 
-const MineSearch = () => {
+const MineSearch = memo(() => {
     const [state, dispatch] = useReducer(reducer, initialState);
+    const { tableData, timer, result, halted } = state;
 
-    const value = useMemo(() => ({ tableData: state.tableData, dispatch }), [state.tableData]);
+    const value = useMemo(() => ({ tableData, halted, dispatch }), [tableData, halted]);
+
+    useEffect(() => {
+        let timer;
+        if (halted === false) {
+            timer = setInterval(() => {
+                dispatch({ type: INCREMENT_TIMER });
+            }, 1000);
+        }
+        return () => {
+            clearInterval(timer);
+        }
+    }, [halted]);
 
     return (
         <TableContext.Provider value={value}>
-            <Form dispatch={dispatch} />
-            <div>{state.timer}</div>
+            <Form />
+            <div>{timer}</div>
             <Table />
-            <div>{state.result}</div>
+            <div>{result}</div>
         </TableContext.Provider>
     )
-};
+});
 
 export default MineSearch;
 
--- 8.지뢰찾기/Tr.jsx
+++ 8.지뢰찾기/Tr.jsx
@@ -1,15 +1,16 @@
-import React, { useContext } from 'react';
+import React, { useContext, memo } from 'react';
+import Td from './Td';
 import { TableContext } from './MineSearch';
-import Td from './Td'
 
-
-const Tr = () => {
+const Tr = memo(({ rowIndex }) => {
     const { tableData } = useContext(TableContext);
     return (
         <tr>
-            {tableData[0] && Array(tableData[0].length).fill().map((td, i) => <Td />)}
+            {tableData[0] && Array(tableData[0].length).fill().map((td, i) =>
+                <Td rowIndex={rowIndex} cellIndex={i} />
+            )}
         </tr>
     )
-};
+});
 
 export default Tr;
